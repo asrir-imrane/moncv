eda98b2aea8ef66156651eb4eed8902fc3dba5b3
asrir-imrane <asr.imrane@gmail.com>
2022-04-10 17:03:52 +0000

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,27 +71,6 @@ tabs.forEach(tab =>{
 
 
 
-/*==================== SERVICES MODAL ====================*/
-const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalCloses = document.querySelectorAll('.services__modal-close')
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}
-
-modalBtns.forEach((modalBtn, i) => {
-    modalBtn.addEventListener('click', () =>{
-        modal(i)
-    })
-})
-
-modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click', () =>{
-        modalViews.forEach((modalView) =>{
-            modalView.classList.remove('active-modal')
-        })
-    })
-})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiperPortfolio = new Swiper('.portfolio__container', {
@@ -108,23 +87,6 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
     },
   });
 
-/*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
-
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets:true,
-    },
-    breakpoints:{
-        568:{
-            slidesPreView: 2,
-        }
-    }
-  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
